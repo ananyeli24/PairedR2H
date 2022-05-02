2ab0e7f757df31b1e46d941dbdc19001e39a1c06
Osvaldo <osvruizgar@gmail.com>
2022-05-02 14:36:44 -0400

    Created an 2 empty arrays for odd and even. A for loop to go over multiple arguements and decide if they are odd or even and push them into the empty arrays accordingly. then we returned the arrays in an object.

--- main.js
+++ main.js
@@ -4,40 +4,21 @@ function oddOrEven (numbers) {
     let odd = [];
     
     for (let i = 0; i < numbers.length; i++){
-        if (numbers[1] % 2 === 0) {
+        if (numbers[i] % 2 === 0) {
             even.push(numbers[i]);
         
         } else {
             odd.push(numbers[i]);
         }
     }
+    // create an object with the odd and even array in it
+    const arrayObject = {
+        even,
+        odd
+      };
+    
+      return arrayObject;
 }
 
-
-
-//
-function splitOddAndEven(numbers) {
-    let odd = [];
-    let even = [];
-  
-    for (let i = 0; i < numbers.length; i++) {
-      if (numbers[i] % 2 === 0) {
-        // number is even
-        even.push(numbers[i]);
-      } else {
-        // number is not even (=odd)
-        odd.push(numbers[i]);
-      }
-    }
-  
-    // create an object with the odd and even array in it
-    const returnObject = {
-      odd,
-      even
-    };
-  
-    return returnObject;
-  }
-
-  console.log(splitOddAndEven([12, 23, 49, 32, 27]));
-  console.log(splitOddAndEven([11,4,2,12,15]));
+  console.log(oddOrEven([12, 23, 49, 32, 27]));
+  console.log(oddOrEven([11,4,2,12,15]));
